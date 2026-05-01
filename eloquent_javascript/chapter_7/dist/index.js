f7807e00a837ad29817b12180d8f599aae25a4f6
LankyLuke97 <lukehowden@rocketmail.com>
2026-05-01 09:11:24 +0100

    Eloquent JS, Chapter 7

--- eloquent_javascript/chapter_7/dist/index.js
+++ eloquent_javascript/chapter_7/dist/index.js
@@ -16,6 +16,12 @@ const roads = [
     "Marketplace-Shop",
     "Shop-Town Hall"
 ];
+const mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House",
+    "Town Hall", "Daria's House", "Ernie's House",
+    "Grete's House", "Shop", "Grete's House", "Farm",
+    "Marketplace", "Post Office"
+];
 function buildGraph(edges) {
     let graph = Object.create(null);
     function addEdge(from, to) {
@@ -74,20 +80,111 @@ class VillageState {
         return new VillageState("Post Office", parcels);
     }
 }
-function runRobot(state, robot, memory) {
+function findRoute(graph, from, to) {
+    let work = [{ at: from, route: [] }];
+    for (let i = 0; i < work.length; i++) {
+        let { at, route } = work[i];
+        for (let place of graph[at]) {
+            if (place == to)
+                return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({ at: place, route: route.concat(place) });
+            }
+        }
+    }
+    return [];
+}
+function runRobot(state, robot, memory = []) {
     for (let turn = 0;; turn++) {
         if (state.parcels.length == 0) {
             console.log(`Done in ${turn} turns`);
-            break;
+            return turn;
         }
         let action = robot(state, memory);
         state = state.move(action.direction);
-        memory = action.memory;
+        memory = action.memory ?? memory;
         console.log(`Moved to ${action.direction}`);
     }
 }
 function randomRobot(state) {
     return { direction: randomPick(roadGraph[state.place]) };
 }
+function mailRobot(state, memory = []) {
+    if (memory.length === 0) {
+        memory = mailRoute;
+    }
+    return { direction: memory[0], memory: memory.slice(1) };
+}
+function goalOrientedRobot(state, route) {
+    if (route.length === 0) {
+        let parcel = state.parcels[0];
+        if (parcel.place != state.place) {
+            route = findRoute(roadGraph, state.place, parcel.place);
+        }
+        else {
+            route = findRoute(roadGraph, state.place, parcel.address);
+        }
+    }
+    return { direction: route[0], memory: route.slice(1) };
+}
+function measureRobots(robot1, robot2, iterations = 100) {
+    let turns1 = 0, turns2 = 0;
+    for (let i = 0; i < iterations; i++) {
+        let state = VillageState.random();
+        turns1 += runRobot(state, robot1);
+        turns2 += runRobot(state, robot2);
+    }
+    console.log(`${robot1.name} took ${turns1 / iterations} turns on average to deliver all the parcels.`);
+    console.log(`${robot2.name} took ${turns2 / iterations} turns on average to deliver all the parcels.`);
+}
 runRobot(VillageState.random(), randomRobot);
+runRobot(VillageState.random(), mailRobot);
+runRobot(VillageState.random(), goalOrientedRobot);
+measureRobots(randomRobot, goalOrientedRobot);
+const SECRET_KEY = Symbol('private');
+class PGroup {
+    #stored;
+    static #empty;
+    constructor(key, data = []) {
+        if (key !== SECRET_KEY) {
+            throw new Error("Cannot create new PGroup. Use PGroup.empty instead");
+        }
+        this.#stored = data;
+    }
+    static get empty() {
+        if (!PGroup.#empty) {
+            PGroup.#empty = new PGroup(SECRET_KEY);
+        }
+        return PGroup.#empty;
+    }
+    add(value) {
+        for (let s of this.#stored) {
+            if (s === value)
+                return this;
+        }
+        return new PGroup(SECRET_KEY, this.#stored.concat(value));
+    }
+    delete(value) {
+        for (let i = 0; i < this.#stored.length; i++) {
+            if (this.#stored[i] === value) {
+                return new PGroup(SECRET_KEY, this.#stored.slice(0, i).concat(this.#stored.slice(i + 1)));
+            }
+        }
+        return this;
+    }
+    has(value) {
+        return this.#stored.some(s => s === value);
+    }
+    toString() {
+        this.#stored.toString();
+    }
+}
+let pgroup = PGroup.empty;
+pgroup = pgroup.add(1);
+pgroup = pgroup.add('2');
+pgroup = pgroup.add(2);
+pgroup = pgroup.add(2);
+console.log(pgroup.has(2));
+pgroup = pgroup.delete('2');
+console.log(!pgroup.has('2'));
 //# sourceMappingURL=index.js.map
--- eloquent_javascript/chapter_7/src/index.ts
+++ eloquent_javascript/chapter_7/src/index.ts
@@ -15,6 +15,13 @@ const roads = [
     "Shop-Town Hall"
 ];
 
+const mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House",
+    "Town Hall", "Daria's House", "Ernie's House",
+    "Grete's House", "Shop", "Grete's House", "Farm",
+    "Marketplace", "Post Office"
+];
+
 function buildGraph(edges: [string, ...string[]]) {
     let graph = Object.create(null);
     function addEdge(from: string, to: string) {
@@ -78,18 +85,33 @@ class VillageState {
     }
 }
 
-type Robot = (state: VillageState, ...args: unknown[]) => {direction: string, memory?: unknown}
+function findRoute(graph: Record<string, string[]>, from: string, to: string) {
+    let work: {at: string, route: string[]}[] = [{at: from, route: []}];
 
-function runRobot(state: VillageState, robot: Robot, memory?: unknown) {
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i]!;
+        for (let place of graph[at]!) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+    return []
+}
+
+type Robot = (state: VillageState, memory: string[]) => {direction: string, memory?: string[]}
+
+function runRobot(state: VillageState, robot: Robot, memory: string[] = []) {
     for (let turn = 0;; turn++) {
         if (state.parcels.length == 0) {
             console.log(`Done in ${turn} turns`);
-            break
+            return turn;
         }
 
         let action = robot(state, memory);
         state = state.move(action.direction);
-        memory = action.memory;
+        memory = action.memory ?? memory;
         console.log(`Moved to ${action.direction}`);
     }
 }
@@ -98,4 +120,96 @@ function randomRobot(state: VillageState) {
     return {direction: randomPick(roadGraph[state.place])}
 }
 
+function mailRobot(state: VillageState, memory: string[] = []) {
+    if (memory.length === 0) {
+        memory = mailRoute;
+    }
+    return {direction: memory[0]!, memory: memory.slice(1)}
+}
+
+function goalOrientedRobot(state: VillageState, route: string[]) {
+    if (route.length === 0) {
+        let parcel = state.parcels[0]!
+        if (parcel.place != state.place) {
+            route = findRoute(roadGraph, state.place, parcel.place)
+        } else {
+            route = findRoute(roadGraph, state.place, parcel.address)
+        }
+    }
+
+    return {direction: route[0]!, memory: route.slice(1)}
+}
+
+function measureRobots(robot1: Robot, robot2: Robot, iterations: number = 100) {
+    let turns1: number = 0, turns2: number = 0;
+    for(let i = 0; i < iterations; i++) {
+        let state = VillageState.random()
+        turns1 += runRobot(state, robot1);
+        turns2 += runRobot(state, robot2);
+    }
+    console.log(`${robot1.name} took ${turns1 / iterations} turns on average to deliver all the parcels.`)
+    console.log(`${robot2.name} took ${turns2 / iterations} turns on average to deliver all the parcels.`)
+}
+
 runRobot(VillageState.random(), randomRobot);
+runRobot(VillageState.random(), mailRobot);
+runRobot(VillageState.random(), goalOrientedRobot);
+
+measureRobots(randomRobot, goalOrientedRobot);
+
+
+
+
+const SECRET_KEY = Symbol('private')
+class PGroup {
+    #stored: any[]
+    static #empty: PGroup | undefined
+
+    constructor(key: Symbol, data: any[] = []) {
+        if (key !== SECRET_KEY) {
+            throw new Error("Cannot create new PGroup. Use PGroup.empty instead");
+        }
+        this.#stored = data
+    }
+
+    static get empty() {
+        if (!PGroup.#empty) {
+            PGroup.#empty = new PGroup(SECRET_KEY);
+        }
+        return PGroup.#empty;
+    }
+
+    add(value: any) {
+        for(let s of this.#stored) {
+            if (s === value) return this;
+        }
+        return new PGroup(SECRET_KEY, this.#stored.concat(value));
+    }
+
+    delete(value: any) {
+        for (let i = 0; i < this.#stored.length; i++) {
+            if (this.#stored[i] === value) {
+                return new PGroup(SECRET_KEY, this.#stored.slice(0,i).concat(this.#stored.slice(i+1)));
+            }
+        }
+        return this;
+    }
+
+    has(value: any) {
+        return this.#stored.some(s => s === value);
+    }
+
+    toString() {
+        this.#stored.toString();
+    }
+}
+
+let pgroup = PGroup.empty
+pgroup = pgroup.add(1)
+pgroup = pgroup.add('2')
+pgroup = pgroup.add(2)
+pgroup = pgroup.add(2)
+console.log(pgroup.has(2))
+pgroup = pgroup.delete('2')
+console.log(!pgroup.has('2'))
+
