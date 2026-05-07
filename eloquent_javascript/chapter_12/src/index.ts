2ad3746955f3a148e23191ec5d4e9941b4bc807d
LankyLuke97 <lukehowden@rocketmail.com>
2026-05-07 19:11:40 +0100

    Eloquent JS, Chapter 12
    Add functions to Egg

--- eloquent_javascript/chapter_12/src/index.ts
+++ eloquent_javascript/chapter_12/src/index.ts
@@ -151,3 +151,39 @@ do(define(total, 0),
    print(total)
   )
 `);
+
+specialForms.fun = (args_param: Expression[], scope: Record<string, Value>) => {
+    if (!args_param.length) throw new SyntaxError("Functions need a body");
+    let args = args_param as [Expression, ...Expression[]]
+    
+    let body = args[args.length-1] as Expression;
+    let params = args.slice(0, args.length-1).map(expr => {
+        if (expr.type != "word") throw new SyntaxError("Parameter names must be words");
+        return expr.name;
+    }) as string[];
+
+    return function(...args: Expression[]) {
+        if (args.length != params.length) throw new TypeError("Wrong number of arguments");
+        let localScope = Object.create(scope);
+        for (let i = 0; i < args.length; i++) localScope[params[i]!] = args[i];
+        return evaluate(body!, localScope);
+    };
+};
+
+run(`
+do(define(plusOne, fun(a, +(a, 1))),
+   print(plusOne(10))
+  )
+`);
+
+run(`
+do(define(pow, fun(base, exp,
+             if(==(exp, 0),
+                1,
+                *(base, pow(base, -(exp, 1))))
+               )
+         ),
+   print(pow(2, 10))
+  )
+`);
+
