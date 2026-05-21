8755477e70577e3e7dbc367b5c041054ad0b189e
LankyLuke97 <lukehowden@rocketmail.com>
2026-05-21 18:55:50 +0100

    Rendering list of items: bug
    Returning JSX for a list of items. Currently code is wrong somewhere.
    Getting an error from the linter on line 17 of:
    
    `Parsing error: Adjacent JSX elements must be wrapped in an enclosing tag`

--- road_to_react/hacker-stores/src/App.jsx
+++ road_to_react/hacker-stores/src/App.jsx
@@ -4,15 +4,23 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-const welcome = {title: "React", greeting: "Hey"};
+const test_list = Array.from(range(0,10));
 
 function App() {
     return (
         <div>
-            <h1>{welcome.greeting} {welcome.title}</h1>
+            <h1>Learning React</h1>
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" />
         </div>
+
+        <hr />
+
+        <ul>
+            {test_list.map(item => {
+                return <li>This is item {item}</li>;
+            });}
+        </ul>
     );
 }
 
